d8d52ed4aff575216c259b5563948163636e1b04
wasim altayyan <wasimtayyan@gmail.com>
2023-03-30 16:40:46 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function add(num1, num2){
+ return num1 + num2;
+}
+function subtract(num1, num2){
+  return num1 - num2;
+}
+function multiply(num1, num2){
+ return  num1 * num2;
+}
+function divide(num1, num2){
+  return num1 / num2;
+}
+let a = 1;
+function increment(a){
+    return (a += 1);
+}
+function decrement(a){
+    return (a -= 1);
+}
+function makeInt(n){
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n,);
+    
+}
+
+preserveDecimal('sldkjflksjf')
+add(500,12);
+add(600,71);
+subtract(280,559)
